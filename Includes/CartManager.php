2c8b52dbc22743915909f3551bd169f6376fd24a
unknown <AskewR04@UNL102484.uniad.unitec.ac.nz>
2016-10-15 16:07:34 +1300

    added business object prototypes for later work

---
+++ Includes/CartManager.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roland
+ * Date: 15/10/2016
+ * Time: 14:24 PM
+ */
+
+namespace BusinessLayer;
+
+// Cart business object.
+
+class CartManager
+{
+	private $_data_manager;	
+	
+	function __construct()
+	{
+		
+	}
+	
+	//TODO: add functionality for managing shopping cart records
+	
+	// ??? Is this necessary? Or interact straight away with SESSION?
+}
